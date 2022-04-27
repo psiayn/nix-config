098d070a2ab7b3f749c72ed2a972fdb76eae98bf
Pranav Kesavarapu <pranavkesavarapu@gmail.com>
2022-04-27 15:02:50 +0530

    fixing lsp

--- programs/neovim/neovim.nix
+++ programs/neovim/neovim.nix
@@ -1,5 +1,25 @@
 with builtins;
 { config, pkgs, ... }: let
+  nvim-lsp-installer = pkgs.vimUtils.buildVimPlugin rec {
+    pname = "nvim-lsp-installer";
+    version = "0bbc4b5402c63e285ce924fbe3f872cb8a5f6595";
+    src = pkgs.fetchFromGitHub {
+      owner = "williamboman";
+      repo = "nvim-lsp-installer";
+      rev = version;
+      sha256 = "dY+Zw/woyl5aqe+l44TltAfnSLGQD2gkZzGTOZl/SP4=";
+    };
+  };
+  rust-tools = pkgs.vimUtils.buildVimPlugin rec {
+    pname = "rust-tools";
+    version = "8922a58459bb6083325327cc26bde94c05b51600";
+    src = pkgs.fetchFromGitHub {
+      owner = "simrat39";
+      repo = "rust-tools";
+      rev = version;
+      sha256 = "dY+Zw/woyl5aqe+l44TltAfnSLGQD2gkZzGTOZl/SP4=";
+    };
+  };
   lsp-zero = pkgs.vimUtils.buildVimPlugin rec {
     pname = "lsp-zero";
     version = "e6f46b68edebbce73a18ab0856df2da703b748b3";
@@ -49,26 +69,31 @@ colorscheme terafox
       nvim-treesitter
       # lsp goodies
       nvim-lspconfig
-      rust-tools-nvim
       cmp-nvim-lsp
       cmp_luasnip
       nvim-cmp
       luasnip
+      nvim-lsp-installer
       lsp-zero
+      rust-tools-nvim
       # general goodies
       gitsigns-nvim
       kommentary
       neogit
       (nvimPlugin telescope-nvim ''
-        nnoremap <leader>ff <cmd>Telescope find_files<cr>
-        nnoremap <leader>fg <cmd>Telescope live_grep<cr>
-        nnoremap <leader>fb <cmd>Telescope buffers<cr>
-        nnoremap <leader>fh <cmd>Telescope help_tags<cr>
+        nnoremap <leader>ff <cmd>lua require('telescope.builtin').find_files()<cr>
+        nnoremap <leader>fg <cmd>lua require('telescope.builtin').live_grep()<cr>
+        nnoremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<cr>
+        nnoremap <leader>fh <cmd>lua require('telescope.builtin').help_tags()<cr>
       '')
       (nvimPlugin twilight-nvim ''
         lua require("twilight").setup{}
         nnoremap <leader>tw <cmd>Twilight<cr>
       '')
     ];
+  };
+  xdg.configFile."nvim/lua" = {
+      recursive = true;
+      source = ./lua;
   };
 }
