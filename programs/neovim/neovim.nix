d97c6dfe4113ba54230db51dea5dbb9ba0e28f8c
Pranav Kesavarapu <pranavkesavarapu@gmail.com>
2022-04-28 11:31:43 +0530

    [neovim] settings syntax as scheme for spressolisp

--- programs/neovim/neovim.nix
+++ programs/neovim/neovim.nix
@@ -45,6 +45,7 @@ in {
     autocmd Filetype lua,javascript,typescript setlocal tabstop=2 shiftwidth=2 softtabstop=2 expandtab
     set splitbelow
     set splitright
+    autocmd BufNewFile,BufRead *.spr set syntax=scheme
   '';
   programs.neovim = {
     enable = true;
